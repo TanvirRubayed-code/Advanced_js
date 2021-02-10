dd1cd1e7be99907cf296b0701fda18f06943a977
Tanvir Rubayed <tanvirrubayedtauhid@gmail.com>
2021-02-10 12:46:42 +0600

    true vs false

---
+++ tureFalse.js
@@ -0,0 +1,17 @@
+const name2 =NaN ;
+
+
+//        Falsy conditions
+//false 
+// 0 
+// ''
+// undefined
+// null 
+// NaN
+
+if(name2) {
+    console.log('Condtion is true ');
+}
+else {
+    console.log('Condition is false ');
+}
