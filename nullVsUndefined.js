4cc890472aec7bdcdc1df08c10c907fdbc17e895
Tanvir Rubayed <tanvirrubayedtauhid@gmail.com>
2021-02-10 12:57:19 +0600

    nullvsUndefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,14 @@
+//let pakhi ;
+
+// function add (num1 , num2){
+//     console.log(num1 + num2) ;
+//     return
+// }
+
+// function add(num1 , num2 ){
+//     console.log(num1 , num2);
+// }
+
+const premik = {name: 'smart muno', phone:01743443344 };
+
+console.log(premik.gf);
