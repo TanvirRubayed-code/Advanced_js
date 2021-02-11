7e87492be85d1c7ffa75595a73e7c8ecd10d9687
Tanvir Rubayed <tanvirrubayedtauhid@gmail.com>
2021-02-11 22:55:53 +0600

    changed closure

--- closure.js
+++ closure.js
@@ -9,11 +9,12 @@ const clock1 = stopWatch();
 console.log(clock1());
 console.log(clock1());
 console.log(clock1());
-console.log(clock1());
-console.log(clock1());
+
 
 const clock2 = stopWatch();
 console.log(clock2());
 console.log(clock2());
+console.log(clock1());
+console.log(clock1());
 console.log(clock2());
 
